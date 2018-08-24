5adff55e8383aae1f4a736e35cdbfd4b767d622a
webmasterdevlin <webmasterdevlin@gmail.com>
2018-08-24 15:13:46 +0800

    authorize added on UsersController

--- JwtAngularScaffold/Controllers/UsersController.cs
+++ JwtAngularScaffold/Controllers/UsersController.cs
@@ -2,12 +2,14 @@
 using AutoMapper;
 using JwtAngularScaffold.Contracts;
 using JwtAngularScaffold.Models.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JwtAngularScaffold.Controllers
 {
     [Produces("application/json")]
     [Route("api/users")]
+    [Authorize]
     public class UsersController : Controller
     {
         private readonly IUserRepository _repo;
